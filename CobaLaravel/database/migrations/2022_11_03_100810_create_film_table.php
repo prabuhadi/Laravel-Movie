7704c0a5af8d873177f90905cb2755ed1707d71b
prabuhadi <prabuhadiyo@gmail.com>
2022-11-03 22:46:36 +0700

    Selesai mengerjakan migration database table

---
+++ CobaLaravel/database/migrations/2022_11_03_100810_create_film_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilmTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('film', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('ringkasan');
+            $table->integer('tahun');
+            $table->string('poster');
+            $table->unsignedBigInteger('genre_id');
+ 
+            $table->foreign('genre_id')->references('id')->on('genre')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
